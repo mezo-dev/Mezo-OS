6bcc2faeb8bf84940221fc1949ea0f820d60e45b
Moataz0000 <moatazfawzy150@gmail.com>
2026-04-15 00:45:11 +0200

    feat: setup workflow

--- kernel/process/__init__.py
+++ kernel/process/__init__.py
@@ -1,13 +1,3 @@
 from .process_manager import ProcessManager
-from pprint import pprint
-
-pm = ProcessManager()
-
-pm.create_process(name="init process")
-pm.create_process(name="shell process")
-pm.create_process(name="user process")
-
-
-
-pprint(pm.list_processes())
-print(pm.get_process_by_pid(pid=1))
+from .process import Process
+from .process_state import ProcessState
